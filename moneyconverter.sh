a9d8d61380c3f001ced3290912510ab7358802d4
Luan Pereira <luanpsmorris@gmail.com>
2020-09-07 22:19:02 -0300

    Added Choice
    Added conditional to choose between BRL and USD.

--- moneyconverter.sh
+++ moneyconverter.sh
@@ -11,11 +11,29 @@ cot=$(wget -qO- https://investing.com/currencies/usd-brl | grep last_last | sed 
 echo -e Cotação do Dolar hoje $(date +"%d/%m/%Y") é R\$$cot.
 echo ""
 
-echo "Digite o valor:"
-read value
-
-conv=$(bc <<< "scale=4; $value / $cot")
-
+echo "Converter de:"
+echo "1. BRL para USD"
+echo "2. USD para BRL"
 echo ""
-echo "R\$$value equivale à U\$$conv"
+
+echo "Digite sua opção:"
+read choice
+echo ""
+
+echo "Digite o valor a ser convertido:"
+read value
+echo ""
+
+brlusd=$(bc <<< "scale=4; $value / $cot")
+usdbrl=$(bc <<< "scale=4; $value * $cot")
+
+if [ $choice == 1 ];
+then 
+	echo "R\$$value equivale à U\$$brlusd"
+elif [ $choice == 2 ];
+then 
+	echo "U\$$value equivale à R\$$usdbrl"
+else 
+	echo "Opção inválida."
+fi
 
