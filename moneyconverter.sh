e0b0bdbc2c6c9eaf985ee5becd42743bba8202c6
Luan Pereira <luanpsmorris@gmail.com>
2020-09-07 21:10:55 -0300

    Added title "MONEY CONVERTER"

--- moneyconverter.sh
+++ moneyconverter.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
+echo ""
+echo "****************************************"
+echo "*           MONEY CONVERTER            *"
+echo "****************************************"
+echo ""
+
 cot=$(wget -qO- https://investing.com/currencies/usd-brl | grep last_last | sed -e 's/<[^>]*>//g')
 
 echo -e Cotação do Dolar hoje $(date +"%d/%m/%Y") é R\$$cot.
+echo ""
 
 echo "Digite o valor:"
 read value
 
 conv=$(bc <<< "scale=4; $value / $cot")
 
+echo ""
 echo "R\$$value equivale à U\$$conv"
 
